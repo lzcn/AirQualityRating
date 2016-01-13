dd159e5672d9d6cb29fc90cc2f67f43cbb273c4f
LuZhi <luzhi92@163.com>
2016-01-13 10:49:29 +0800

    modify

--- Extract_All_Features.m
+++ Extract_All_Features.m
@@ -1,33 +1,43 @@
 % paras:
 %   folder: image data which in '/data/folder' has two folders 'TraningSet' and 'TestSet'
 %   flag : whether to save the feature image;
-%   feature_func : select which features to exrract;
+%   feature_func : select which features to extract;
 %   feature_file : the coresponding file to save feature vectors
 %                  training set will be save as 'train_'+ feature_file;
 %                  while test set as 'test_' + feature_file;
-function interval = Extract_All_Features(folder,flag,feature_func,feature_file)
-%% extract the origin features for training set 
-TwoSet = {'TraningSet','TestSet'};
+function eaf = Extract_All_Features(folder,flag,feature_func,feature_file,feature_file_dir)
+%% extract the origin features
+TwoSets = {'TraningSet','TestSet'};
 prefix = {'train_','test_'};
-for k=1:2
-    for i = 1:length(feature_func)
-        dataset = set_image_data(folder,TwoSet{k});
-        if dataset == -1 
-            return;
-        end
-        feval(feature_func{i},dataset,feature_file{i},flag);
+
+if length(feature_func) ~= length(feature_file)
+    eaf = -1;
+    fprintf('>> function''s length doesn''t matchs file''s\n');
+    return;
+else NumF = length(feature_func);
+end
+for i = 1:2
+    feature_fullname = strcat(prefix{1},feature_file(:));
+    dataset = set_image_data(folder,TwoSets{i});
+    if dataset == -1
+        eaf = -1;
+        return;
+    end
+    fprintf('>> Extract %s''s Features\n',TwoSets{i});
+    parfor j = 1:length(feature_func)
+        feval(feature_func{j},dataset,feature_fullname{j},flag);
     end
 end
-Train_dataset = set_image_data(folder,'TrainingSet');
-disp('>>Extract TrainingSet Features');
-
-origin_train = {'train_transmission_origin','train_pss_origin','train_contrast_origin','train_saturation_origin'};
-ExtractFeature_Transmission(Train_dataset,'train_transmission_origin',flag);
-ExtractFeature_pss(Train_dataset,'train_pss_origin',flag);
-ExtractFeature_contrast(Train_dataset,'train_contrast_origin',flag);
-ExtractFeature_satu(Train_dataset,'train_saturation_origin',flag);
+% Train_dataset = set_image_data(folder,'TrainingSet');
+% disp('>>Extract TrainingSet Features');
+% 
+% origin_train = {'train_transmission_origin','train_pss_origin','train_contrast_origin','train_saturation_origin'};
+% ExtractFeature_Transmission(Train_dataset,'train_transmission_origin',flag);
+% ExtractFeature_pss(Train_dataset,'train_pss_origin',flag);
+% ExtractFeature_contrast(Train_dataset,'train_contrast_origin',flag);
+% ExtractFeature_satu(Train_dataset,'train_saturation_origin',flag);
 %% generate interval fo hist and rewrite the feature file
-interval = cell(length(origin_train),1);
+interval = cell(NumF,1);
 % the directory storing the feature files
 feature_file_dir = fullfile('result',folder,'Features');
 for kk =1:length(origin_train)
